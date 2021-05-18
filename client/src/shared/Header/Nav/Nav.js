9622e0b810e4d9ecacc3bf899975a0d72980db2f
Nick Scardino <nick@scardino.dev>
2021-05-18 18:37:47 -0400

    more style tweaks for mobile landscape

--- client/src/shared/Header/Nav/Nav.js
+++ client/src/shared/Header/Nav/Nav.js
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 const Nav = ({close}) => {
     return (
         <nav>
-            <NavLink exact to={'/calendar'} className={'navBtn'} activeClassName={'selected'}>
+            <NavLink exact to={'/calendar'} activeClassName={'selected'}>
                 <Button name={"Calendar"} action={close} />
             </NavLink>
-            <NavLink exact to={'/library'} className={'navBtn'} activeClassName={'selected'}>
+            <NavLink exact to={'/library'} activeClassName={'selected'}>
                 <Button name={"Crop Library"} action={close} />
             </NavLink>
-            <NavLink exact to={'/schedule'} className={'navBtn'} activeClassName={'selected'}>
+            <NavLink exact to={'/schedule'} activeClassName={'selected'}>
                 <Button name={"Scheduler"} action={close} />
             </NavLink>
         </nav>
